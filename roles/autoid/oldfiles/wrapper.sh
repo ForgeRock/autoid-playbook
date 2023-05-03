1b1176929200ea3d5aa3bc1990c6586d3011c0dd
hope.sun <hope.sun@forgerock.com>
2023-05-03 16:40:50 -0400

    remove old files

--- roles/autoid/oldfiles/wrapper.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
- 
-usermod -aG wheel autoid
-echo 'autoid ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/autoid
-su - autoid &
-cd /home/autoid/autoid-config
-sudo /home/autoid/autoid-config/deployer.sh run
-gpasswd -d autoid wheel
-rm /etc/sudoers.d/autoid
